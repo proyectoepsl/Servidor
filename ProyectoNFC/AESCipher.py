7f6d37c45c5292edf2721fddc0a198f68abaaec9
proyectoepsl <proyectoepsl@gmail.com>
2017-07-16 20:52:08 +0200

    Rest usuario terminado androip

--- ProyectoNFC/AESCipher.py
+++ ProyectoNFC/AESCipher.py
@@ -5,24 +5,24 @@ from Cryptodome import Random
 
 
 class AESCipher:
-
+    #Encriptacion de la clave
     def __init__(self):
         key = b'this is my key'
         self.bs = int(16)
         self.key = hashlib.sha256(key).digest()
-
+    #Encriptacion de los datos
     def encrypt(self, message):
         message = self._pad(message)
         iv = Random.new().read(AES.block_size)
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
         return base64.b64encode(iv + cipher.encrypt(message)).decode('utf-8')
-
+    #Desencritar los datos
     def decrypt(self, enc):
         enc = base64.b64decode(enc)
         iv = enc[:AES.block_size]
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
         return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
-
+    #Formateo de los datos
     def _pad(self, s):
         print(len(s))
         print(self.bs)
@@ -33,15 +33,3 @@ class AESCipher:
     @staticmethod
     def _unpad(s):
         return s[:-ord(s[len(s)-1:])]
-'''
-key = b'this is my key'
-mensaje = b'this is local test'
-
-mensajeEncriptado=AESCipher(key).encrypt(mensaje)
-print(mensajeEncriptado)
-
-mensajeEncriptado=b'X9G5cBZ5/Hvcqez9fjsIM6RARpYUnoTOb5cdCW9FzJFhKcA+2EEsl0pEpBVNBSMr'
-
-mensajeDesencriptado=AESCipher(key).decrypt(mensajeEncriptado)
-print(mensajeDesencriptado)
-'''
